1779613428457ba53653d9c2502b3babb5565abc
Mahalingam1999 <60708491+Mahalingam1999@users.noreply.github.com>
2020-02-14 16:08:05 +0530

    Add files via upload

---
+++ lab-js-21-story-time/src/app.js
@@ -0,0 +1,108 @@
+//Trial 1:
+//We've got some basic info about Karen's home
+//Debug the type of data provided
+//Return the types concatenated in a single variable
+
+function moreAboutHome(address,distanceFromTown,hasNeighbours)
+{
+    let ad=typeof address;
+    let di=typeof distanceFromTown;
+    let hn=typeof hasNeighbours;
+   return ad+di+hn;
+}
+
+//Trial 2:
+//Check if the data given is of the right type
+//parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+function moreAboutKaren(parents,noOfSiblings,isNuclearFamily)
+{
+    if(typeof(parents)=="string" && typeof(noOfSiblings) =="number" && typeof(isNuclearFamily)=="boolean")
+        return true;
+    else
+        return false;
+}
+
+//Trial 3:
+//Lily is suspicious about Karen's new friend
+//Karen tells her friend's age and even writes it down
+//Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(ageInText,ageInNumber){
+    if(isNaN(ageInText))
+    return  ageInText;
+    else
+    return ageInNumber;
+    
+}
+//Trial 4:
+//Lily gave Karen x sweets
+//Karen ate y sweets herself
+//On her way to the river, she ate another z sweets every n meters travelled
+//Her friend divided the remaining sweets into 2 parts for each
+//How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets,sweetsConsumedByKaren,sweetsConsumedInNMeters,metersToTravel)
+{
+    if(typeof(totalNoOfSweets) !="number" || typeof(sweetsConsumedByKaren) != "number" || typeof(sweetsConsumedInNMeters) != "number" || typeof(metersToTravel)!="number")
+    return "No sweets for Karen's friend";
+    else 
+    return ((totalNoOfSweets-sweetsConsumedByKaren-(sweetsConsumedInNMeters*metersToTravel))/2)
+}
+//Trial 5:
+//As Lily moves closer, it gets colder. She checks the temperature on her mobile
+//It only shows in fahrenheit. Convert the data to celsius and return it.
+
+function convertToCelsius(fahrenheit)
+{
+    if(typeof(fahrenheit) == "undefined" || typeof(fahrenheit) =="string" || typeof(fahrenheit) =="object")
+    return 'Technical Error!';
+    else
+return ((fahrenheit-32)/1.8);
+}
+//Trial 6:
+//Lily can now do multiple things to deal with this
+//1. Take her daughter to a doctor
+//2. Talk to her husband about it
+//3. Counsel her daughter herself
+//4. Lock her daughter in her room
+//Given a value, return which of these above actions Lily would take
+
+function aDifficultChoice(choice)
+{
+
+    if(choice == 1 )
+        return "Take her daughter to a doctor";
+    else if(choice== -1)
+        return 'Break down and give up all hope';
+    else if(choice == 2)
+        return 'Talk to her husband about it';
+    else if(choice == 3)
+        return 'Counsel her daughter herself';
+    else if(choice == 4)
+        return 'Lock her daughter in her room';
+    else if(typeof(choice) == "undefined")
+         return "Wasn't able to decide";
+    else if(choice == "I give up" )
+            return 'Refused to do anything for Karen';
+}
+//Challenge 1:
+//Lily realized that she'd hurt her daughter
+//All she wants now is for her to stop crying
+//She refuses to talk to her but Lily doesn't stop trying
+//She tries out multiple things hoping for the best
+//Take all of Lily's strategies and concatenate them to a single var
+//Seperate the strategies by a single space
+//Return the length of the complete strategy
+
+function consoleKaren(strategies)
+{
+    
+    let strategy1="Lily realized that she'd hurt her daughter";
+    let strategy2="All she wants now is for her to stop crying";
+    let strategy3="She refuses to talk to her but Lily doesn't stop trying";
+    let strategy4="She refuses to talk to her but Lily doesn't stop trying";
+    // let allstrategy=strategy1+strategy2+strategy3+strategy4;
+    let seperate=strategy1+" "+strategy2+" "+strategy3+" "+strategy4;
+    // return seperate;
+    return seperate.length;
+}
